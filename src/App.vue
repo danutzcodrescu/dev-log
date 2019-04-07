a459ca09720a783ea018ccacedb99483b7931cf6
danutzcodrescu <danutzcodrescu@gmail>
2019-04-07 17:26:48 +0200

    refactor: create card component

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,6 @@
   padding: 20px;
 }
 
-.card {
-  border: 1px solid #d3d3d3;
-  padding: 15px;
-  margin-bottom: 15px;
-  box-shadow: inset 0 0 10px darkgrey;
-}
-
 .grid {
   display: grid;
 }
---
+++ src/components/Card.component.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Card extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border: 1px solid #d3d3d3;
+  padding: 15px;
+  margin-bottom: 15px;
+  box-shadow: inset 0 0 10px darkgrey;
+}
+</style>
+
+
